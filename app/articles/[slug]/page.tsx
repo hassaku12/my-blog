5da57b689fa46d85d2569b13db31f6319f5419a1
hassaku12 <you.so.7173@gmail.com>
2023-10-10 23:29:48 +0900

    MetaData

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -1,22 +1,25 @@
-import { Metadata } from 'next';
-import { getDetail } from '@/libs/microcms';
-import Article from '@/components/Article';
+import { Metadata } from 'next'
+import { getDetail } from '@/libs/microcms'
+import Article from '@/components/Article'
 
 type Props = {
   params: {
-    slug: string;
-  };
+    slug: string
+  }
   searchParams: {
-    dk: string;
-  };
-};
+    dk: string
+  }
+}
 
-export const revalidate = 60;
+export const revalidate = 60
 
-export async function generateMetadata({ params, searchParams }: Props): Promise<Metadata> {
+export async function generateMetadata({
+  params,
+  searchParams,
+}: Props): Promise<Metadata> {
   const data = await getDetail(params.slug, {
     draftKey: searchParams.dk,
-  });
+  })
 
   return {
     title: data.title,
@@ -26,13 +29,13 @@ export async function generateMetadata({ params, searchParams }: Props): Promise
       description: data.description,
       images: [data?.thumbnail?.url || ''],
     },
-  };
+  }
 }
 
 export default async function Page({ params, searchParams }: Props) {
   const data = await getDetail(params.slug, {
     draftKey: searchParams.dk,
-  });
+  })
 
-  return <Article data={data} />;
+  return <Article data={data} />
 }
